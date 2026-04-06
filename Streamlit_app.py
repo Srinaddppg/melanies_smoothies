e90a6b887587eeb080cb5f242cb15171827d35d0
Srinaddppg <srina1501268@mastek.com>
2026-04-06 10:06:02 +0100

    Fix typo in use_container_width parameter

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -50,7 +50,7 @@ try:
   smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
   st.text(smoothiefroot_response.status_code)
   st.text(smoothiefroot_response.json())
-  sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=true)
+  sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
   st.text(sf_df)
 except Exception as e:
   st.write('error is : ', {e})
