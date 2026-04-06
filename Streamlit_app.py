20220e7a6a262c51d290f73e16b5a43ee59c632b
Srinaddppg <srina1501268@mastek.com>
2026-04-06 09:58:49 +0100

    Fix error message formatting in Streamlit app

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -52,7 +52,7 @@ try:
   st.text(smoothiefroot_response.text)
   sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=true)
 except Exception as e:
-  st.write('error is {e]')
+  st.write('error is : {e}')
     
   #st.write(ingredient_list)
   #st.text(ingredient_list)
