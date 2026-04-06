674aed6be57f655f2cec4b3c5867cfa654b8500d
Srinaddppg <srina1501268@mastek.com>
2026-04-06 07:12:06 +0100

    Create Streamlit app.py
    This Streamlit app allows users to customize their smoothie by selecting fruits and entering a name for the order. It connects to a Snowflake database to retrieve fruit options and submit orders.

---
+++ Streamlit_app.py
@@ -0,0 +1,52 @@
+# Import python packages.
+import streamlit as st
+from snowflake.snowpark.context import  get_active_session
+from snowflake.snowpark.functions import  col
+
+# Write directly to the app.
+st.title(f":cup_with_straw: Customize Your Smoothie:cup_with_straw:")
+st.write(
+  """Choose the fruits you want in custom smoothie
+  """
+)
+
+#option = st.selectbox('What is the Fruit?', ('Banana' + ':banana:', 'Strawberries' + ':strawberries:','Peaches'+ ':peaches:'))
+
+#st.write('Your favorite friut is ', option )
+name_on_order = st.text_input('Name on Smoothie')
+st.write(' The name on smoothie will be :', name_on_order)
+
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.fruit_options") .select(col('FRUIT_NAME'))
+#st.dataframe(data=my_dataframe, use_container_width=True)
+
+ingredient_list = st.multiselect('Choose up the ingredients', 
+                                 my_dataframe,
+                                 max_selections=5)
+#st.write(ingredient_list)
+
+if ingredient_list:
+    ingredients_string = ''
+    for fruits_choosen in ingredient_list:
+        ingredients_string += fruits_choosen +' ';
+    st.write(ingredients_string)
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+                    values ('""" + ingredients_string + "" "', '""" + name_on_order + "" "')"""
+
+    st.write(ingredient_list)
+    
+    time_to_insert = st.button('Submit Order')
+    
+    #st.write(my_insert_stmt)
+    if time_to_insert:
+        #st.write('inserted properly')
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered!', icon="✅")
+
+    
+  #st.write(ingredient_list)
+  #st.text(ingredient_list)
+    
+    
+      
+      
