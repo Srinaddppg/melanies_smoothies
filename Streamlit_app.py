6536f3869a7e524571849c9d50027d86fddc221d
Srinaddppg <srina1501268@mastek.com>
2026-04-06 12:51:48 +0100

    Fix variable name in search value output

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -57,7 +57,7 @@ try:
     for fruits_choosen in ingredient_list:
       ingredients_string += fruits_choosen +' '
       search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruits_choosen, 'SEARCH_ON'].iloc[0]
-      st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+      st.write('The search value for ', fruits_choosen,' is ', search_on, '.')
       st.subheader(fruits_choosen +' Nutrition information')
       smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + {search_on})
       sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
