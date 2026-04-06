8baaa2ee880a130c79ea81a450f1b7aaa7652bdd
Srinaddppg <srina1501268@mastek.com>
2026-04-06 10:06:31 +0100

    Change text display to write for DataFrame output

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -51,7 +51,7 @@ try:
   st.text(smoothiefroot_response.status_code)
   st.text(smoothiefroot_response.json())
   sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-  st.text(sf_df)
+  st.write(sf_df)
 except Exception as e:
   st.write('error is : ', {e})
     
